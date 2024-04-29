038734bc5bc84428a032d0e2ece305db5469a82e
Kiran H Nair <drtech859@gmail.com>
2024-04-29 12:02:36 +0530

    stripe patch update

--- actions/stripe-redirect/index.ts
+++ actions/stripe-redirect/index.ts
@@ -53,7 +53,7 @@ const handler = async (data: InputType): Promise<ReturnType> => {
                         price_data : {
                             currency : "USD",
                             product_data : {
-                                name : "Collabspheere Pro",
+                                name : "Collabsphere Pro",
                                 description : "Unlimited boards for your organization"
                             },
                             unit_amount:500,
